710b80ead51031ad90a2ecafcbb9f0112652b6a8
DanikingMC <75268237+DanikingMC@users.noreply.github.com>
2021-04-22 19:58:46 -0400

    Delete main directory

--- main/java/daniking/throwabletorch/item/ItemThrowableSlimeTorch.java
+++
@@ -1,8 +0,0 @@
-package daniking.throwabletorch.item;
-
-public class ItemThrowableSlimeTorch extends ItemThrowableTorch{
-
-    public ItemThrowableSlimeTorch(Settings settings) {
-        super(settings, Type.SLIME);
-    }
-}
